af8e58e1abfc9cda303a3e354a2017ffbe3bb702
Tofeeq7007 <tofeeqraja9100@gmail.com>
2025-07-15 17:52:46 +0530

    C:/Program Files/Git/ route de deiya

--- app.js
+++ app.js
@@ -24,6 +24,9 @@ app.use(express.urlencoded({extended:true}))
 app.use("/api/create" , router);
 app.use("/api/auth" , authRoutes);
 app.use('/api/user' , User_router)
+app.use("/", (req,res)=>{
+    res.send("Welcome frontend")
+})
 app.get("/:id", redirectFromShortUrl)
 app.listen(process.env.PORT,()=>{
     connectDb();
