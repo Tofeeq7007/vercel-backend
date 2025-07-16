92a0602ac710456aa1e79344e58d969438eba8fb
Tofeeq7007 <tofeeqraja9100@gmail.com>
2025-07-16 22:36:51 +0530

    BE 9

--- src/config/config.js
+++ src/config/config.js
@@ -1,14 +1,7 @@
-// export const cookieOption = {
-//     httpOnly:true,
-//     secure: process.env.NODE_ENV === "production",
-//     sameSite:"lax",
-//     maxAge:1000*60*15, // 5 min
-//     path:"/"
-// }
 export const cookieOption = {
-  httpOnly: true,
-  secure: true,           // ✅ required on Vercel + Render (HTTPS)
-  sameSite: "None",       // ✅ required for cross-site cookies
-  maxAge: 1000 * 60 * 15, // 15 minutes
-  path: "/"
-};
+    httpOnly:true,
+    secure: process.env.NODE_ENV === "production",
+    sameSite:"lax",
+    maxAge:1000*60*15, // 5 min
+    path:"/"
+}
