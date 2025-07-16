8b4a8685c7883879f3b7d250f8f9d422a39e558d
Tofeeq7007 <tofeeqraja9100@gmail.com>
2025-07-16 19:46:46 +0530

    BE PK HARD CODE 8

--- src/config/config.js
+++ src/config/config.js
@@ -1,7 +1,14 @@
+// export const cookieOption = {
+//     httpOnly:true,
+//     secure: process.env.NODE_ENV === "production",
+//     sameSite:"lax",
+//     maxAge:1000*60*15, // 5 min
+//     path:"/"
+// }
 export const cookieOption = {
-    httpOnly:true,
-    secure: process.env.NODE_ENV === "production",
-    sameSite:"lax",
-    maxAge:1000*60*15, // 5 min
-    path:"/"
-}
+  httpOnly: true,
+  secure: true,           // ✅ required on Vercel + Render (HTTPS)
+  sameSite: "None",       // ✅ required for cross-site cookies
+  maxAge: 1000 * 60 * 15, // 15 minutes
+  path: "/"
+};
