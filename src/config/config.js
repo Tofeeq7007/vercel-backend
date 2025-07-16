cecef75771b605aba7d01b68daca7363bd197eac
Tofeeq7007 <tofeeqraja9100@gmail.com>
2025-07-16 23:26:26 +0530

    Final commit 2

--- src/config/config.js
+++ src/config/config.js
@@ -1,14 +1,7 @@
-// export const cookieOption = {
-//     httpOnly:true,
-//     secure: process.env.NODE_ENV === "production",
-//     sameSite:"lax",
-//     maxAge:1000*60*15, // 5 min
-//     path:"/"
-// }
-res.cookie("accessToken", token, {
+export const cookieOption = {
   httpOnly: true,
   secure: true,           // ✅ required for cross-site cookies over HTTPS
   sameSite: "none",       // ✅ allow cross-site cookies
   path: "/",
   maxAge: 15 * 60 * 1000  // 15 minutes
-});
+}
